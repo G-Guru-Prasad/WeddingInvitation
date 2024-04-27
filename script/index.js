377c408132d26d158f6d8afdf604bb8e5c2d6191
Guru Prasad G <guruprasad1704@gmail.com>
2024-04-27 10:51:20 +0530

    Initial set up

---
+++ script/index.js
@@ -0,0 +1,51 @@
+var countDownDate = new Date("Jan 01, 2024 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  // document.getElementById("timerrr").innerHTML = days + "d " + hours + "h "
+  // + minutes + "m " + seconds + "s remaining";
+
+  document.querySelector("#Days .value").innerText = days;
+  document.querySelector("#Hours .value").innerText = hours;
+  document.querySelector("#Minutes .value").innerText = minutes;
+  document.querySelector("#Seconds .value").innerText = seconds;
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countDown").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  const uluru = { lat: -25.344, lng: 131.031 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 14,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
+
+window.initMap = initMap;
