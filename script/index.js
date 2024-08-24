b7628da3f25f47f54b56324d13212ece33825882
Guru Prasad G <guruprasad1704@gmail.com>
2024-08-24 18:34:53 +0530

    Bug fixing

--- script/index.js
+++ script/index.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("May 22, 2024 00:00:00").getTime();
+var countDownDate = new Date("May 22, 2099 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
